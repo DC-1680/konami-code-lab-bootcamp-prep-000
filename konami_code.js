ee072ed50169a4fb0128a903b8004afcabeabd39
Daryl Concha <concha.daryl@gmail.com>
2018-04-01 16:49:36 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
+function init(e) {
   // Write your JavaScript code inside the init() function
+  const key = parseInt(e.detail || e.which);
 
+  let index = 0;
+
+  if (key === alphabet[index]) {
+    index++;
+
+    if (index === alphabet.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
+/*
+const input = document.querySelector('input')
+
+input.addEventListener('keydown', function(e) {
+  console.log(e.which)
+})
+*/
